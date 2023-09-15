2e93f907b9cbd9aa53fc4354663618002a4c90ca
Pietro Ballarin <pieballa@gmail.com>
2023-09-15 02:31:19 +0200

    dev(login): try catch error handling

--- src/login.php
+++ src/login.php
@@ -8,60 +8,67 @@ require_once __DIR__ . '/utils/database.php';
 $ERROR = null; // Variabile per registrare eventuali errori di accesso
 
 // Se l'utente è già loggato lo reindirizzo alla dashboard.
-if (isset($_SESSION['user_email'])) {
+if (isset($_SESSION['user_id'])) {
     header('Location: /dashboard.php');
+    exit();
 }
 
-// Verifico che la richiesta non sia riprodotta
-if (isset($_POST['nonce'])) {
+// Verifico se è stata inoltrata una richiesta di login
+if (isset($_POST)) {
+    // E' stata inoltrata una richiesta di login
+    // pocedo nella gestione della stessa
 
-    if (Nonce::verify($_POST['nonce'])) {
-        // Il nonce è valido la richiesta non è riprodotta quindi procedo.
+    try {
 
-        $submitted_email = $_POST['email'];
-        $submitted_password = $_POST['password'];
+        // Estrapolo i dati della richiesta
+        // e li inserisco in un array associativo
+        // $submitted_user
+        $submitted_user["nonce"] = $_POST["nonce"] ?? null;
+        $submitted_user["email"] = $_POST["email"] ?? null;
+        $submitted_user["password"] = $_POST["password"] ?? null;
 
-        // Verifico che non ci sia un account già registrato con la stessa email.
-        $result = $connection->query("SELECT email, salt, hash FROM users WHERE email = '$submitted_email'");
-        
-        // Se la ricerca ha prodotto risultati allora esiste un account
-        // associato all'email inserita.
-        
-        if ($result->num_rows> 0) {
-            // Estraggo i dati dell'utente.
-            $db_stored_user = $result->fetch_assoc();
-            // Estraggo il salt e l'hash.
-            $db_user_salt = $db_stored_user['salt'];
-            $db_user_hash = $db_stored_user['hash'];
-            // Genero l'hash della password inserita dall'utente insieme al sale.
-            $password_hash = hash('sha256', $submitted_password . $db_user_salt);
-            // Verifico che l'hash della password inserita dall'utente
-            // sia uguale a quello salvato nel database.
-            if ($password_hash === $db_user_hash) {
-                // Se l'hash è uguale allora l'utente ha inserito la password corretta.
-                // Imposto l'id dell'utente nella sessione.
-                $_SESSION['user_email'] = $db_stored_user['email'];
-                // Ora l'utente è loggato quindi posso reindirizzarlo alla dashboard.
-                header('Location: /dashboard.php');
-            } else {
-                // Se l'hash non corrisponde allora l'utente ha inserito la password sbagliata.
-                // E notifico alla pagina che la password è sbagliata per visualizzare un errore.
-                // Reindirizzo l'utente alla pagina di login.
-                $ERROR = 'wrong_password';
-            }
-        } else {
-            // Se la ricerca non ha prodotto risultati allora non esiste un account
-            // associato all'email inserita.
-            // E notifico alla pagina che la password è sbagliata per visualizzare un errore.
-            // Reindirizzo l'utente alla pagina di login.
-            $ERROR = 'account_not_found';
+        // Verifico il nonce contro gli attacchi di replica
+        if (empty($submitted_user["nonce"]) || !Nonce::verify($submitted_user["nonce"])) {
+            // Il nonce non è valido potrebbe essere un attacco
+            // di replica, lancio un eccezione
+            throw new Exception("invalid_nonce");
         }
-    } else {
-        // Il nonce non è valido quindi la richiesta è riprodotta.
-        // Reindirizzo l'utente alla pagina di login.
-        $ERROR = 'replay_attack';
+
+        // Ottengo i dati corrispondenti all'utente dal database
+        $database_user = $connection->query(
+            "SELECT id, salt, hash FROM users
+            WHERE email = '{$submitted_user["email"]}'"
+        )->fetch_assoc();
+
+        // Verifico che l'utente sia registrato nel KDC
+        if (empty($database_user)) {
+            // L'utente non è registrato, informo l'utente
+            // attraverso un'eccezione
+            throw new Exception("account_not_found");
+        }
+
+        $submitted_user["hash"] = hash('sha256', $submitted_user["password"] . $database_user["salt"]);
+
+        // Verifico che gli hash combacino
+        if ($submitted_user["hash"] !== $database_user["hash"]) {
+            // Gli hash non combaciano, quindi la password
+            // è errata, notifico l'utente
+            throw new Exception("wrong_password");
+        }
+
+        // La password è corretta e l'utente è loggato
+        // con successo, salvo l'id utente nella sessione
+        // e lo reindirizzo alla dashboard
+        $_SESSION["user_id"] = $database_user["id"];
+        header("Location: /dashboard.php");
+
+    } catch (Exception $e) {
+        // Salvo il messaggio di errore in una 
+        // variabile per visualizzarlo nella pagina
+        $ERROR = $e->getMessage();
     }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -80,7 +87,7 @@ if (isset($_POST['nonce'])) {
         <a href="/index.php">Indietro</a>
 
         <h1>Login</h1>
-            
+
         <?php
         // Controllo se l'utente è stato appena registrato.
         if (isset($_GET["success"]) && $_GET["success"] == "account_created") {
@@ -90,8 +97,8 @@ if (isset($_POST['nonce'])) {
         // Controllo se c'è qualche messaggio di errore da visualizzare.
         if (isset($ERROR)) {
             switch ($ERROR) {
-                case 'replay_attack':
-                    echo '<div class="error">Richiesta riprodotta!</div>';
+                case 'invalid_nonce':
+                    echo '<div class="error">Il nonce non è valido!</div>';
                     break;
                 case 'account_not_found':
                     echo '<div class="error">Account non trovato!</div>';
@@ -103,7 +110,7 @@ if (isset($_POST['nonce'])) {
         }
 
         ?>
-        
+
         <form method="POST">
             <!--
                 Per rendere sicuro il form dalla da attacchi di replica facciamo uso di un nonce.
