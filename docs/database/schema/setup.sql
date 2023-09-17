fbd772d9512cba74a4e57db2a535f4780f7dc579
Pietro Ballarin <pieballa@gmail.com>
2023-09-18 01:38:12 +0200

    docs(database): setup sql script

--- docs/database/schema/setup.sql
+++ docs/database/schema/setup.sql
@@ -1,4 +1,7 @@
-CREATE TABLE users (
+CREATE DATABASE IF NOT EXISTS itt_kdc;
+USE itt_kdc;
+
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER AUTO_INCREMENT NOT NULL,
 	email VARCHAR(254) NOT NULL,
     salt VARCHAR(32) NOT NULL,
@@ -7,18 +10,18 @@ CREATE TABLE users (
     UNIQUE (email)
 );
 
-CREATE TABLE conversations (
+CREATE TABLE IF NOT EXISTS conversations (
     id INTEGER AUTO_INCREMENT NOT NULL,
     from_user INTEGER NOT NULL,
     to_user INTEGER NOT NULL,
     shared_key VARCHAR(64) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (from_user, to_user),
-    FOREIGN KEY conversations(from_user) REFERENCES users(id),
-    FOREIGN KEY conversations(to_user) REFERENCES users(id)
+    FOREIGN KEY (from_user) REFERENCES users(id),
+    FOREIGN KEY (to_user) REFERENCES users(id)
 );
 
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS messages (
     id INTEGER AUTO_INCREMENT NOT NULL,
     conversation INTEGER NOT NULL,
     datetime DATETIME NOT NULL,
@@ -26,6 +29,6 @@ CREATE TABLE messages (
     sender INTEGER NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (conversation, datetime),
-    FOREIGN KEY messages(conversation) REFERENCES conversations(id),
-    FOREIGN KEY messages(sender) REFERENCES users(id)
+    FOREIGN KEY (conversation) REFERENCES conversations(id),
+    FOREIGN KEY (sender) REFERENCES users(id)
 );
